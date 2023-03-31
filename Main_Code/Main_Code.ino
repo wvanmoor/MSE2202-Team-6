a5ff15ece8aa823bfb7c8a0cf5f3ea912bc8628d
wvanmoor <122902907+wvanmoor@users.noreply.github.com>
2023-03-31 18:57:27 -0400

    Update Main_Code.ino

--- Main_Code/Main_Code.ino
+++ Main_Code/Main_Code.ino
@@ -1,132 +1,211 @@
-#include <ESP32Servo.h>
+#include <Adafruit_NeoPixel.h>
+#include <MSE2202_Lib.h>
 
-#define LIFT
-#define BALANCE
-#define PULLEY_ARM
-#define WHEELS
-#define PULLEYS
-#define TRIGGER
-#define ECHO
-#define CONTACT
+//Pin Allocations
 
-int lift_lower = 0;
-int lift_raise = 0;
-int pulley_side = 0;
-int pulley_centre = 0;
+#define MOTOR_1A
+#define MOTOR_1B
+#define MOTOR_2A
+#define MOTOR_2B
+#define MOTOR_3A
+#define MOTOR_3B
+#define MOTOR_4A
+#define MOTOR_4B
 
-int sonar;
-int sonar_jump = 1000;
+#define MOTOR_5A
+#define MOTOR_5B
+#define MOTOR_6A
+#define MOTOR_6B
 
-int drive_mode = 0;
-int reverse_time = 1000;
+#define SERVO_L1
+#define SERVO_L2
+#define SERVO_R
 
-Servo lift;
-Servo balance;
-Servo pulley_arm;
+#define TRIGGER_FRONT
+#define ECHO_FRONT
+#define TRIGGER_REAR
+#define ECHO_REAR
+
+#define LED
+
+#define BUTTON
+
+//Constants
+
+const int Lift1_Raised = 0;
+const int Lift2_Raised = 0;
+const int Lift1_Lowered = 0;
+const int Lift2_Lowered = 0;
+const int Rack_Centre = 0;
+const int Rack_Side = 0;
+
+const int drive_speed = 0;
+const int reverse_distance
+
+//Variables
+
+unsigned int drive_index;
+
+unsigned long ping_jump;
+unsigned long jump_size;
+
+unsigned long timer;
+boolean timeup = false;
+
+//Objects
+
+Motion Bot = Motion();
 
 void setup() 
 {
-  //Setup Servos
-  lift.setPeriodHertz(50);
-  balance.setPeriodHertz(50);
-  pulley_arm.setPeriodHertz(50);
+  Bot.servoBegin("S1",SERVO_L1);
+  Bot.servoBegin("S2",SERVO_L2);
+  Bot.servoBegin("S3",SERVO_L3);
 
-  lift.attach(LIFT, 500, 2500);
-  balance.attach(BALANCE, 500, 2500);
-  pulley_arm.attach(PULLEY_ARM, 500, 2500)
+  pinMode(BUTTON, INPUT_PULLUP);
 
-  //Setup USM
-  pinMode(TRIGGER, OUTPUT);
-  pinMode(ECHO, INPUT);
+  pinMode(LED, OUTPUT);
 
-  sonar = getSonar();
+  pinMode(TRIGGER_FRONT, OUTPUT);
+  pinMode(TRIGGER_REAR, OUTPUT);
+  pinMode(ECHO_FRONT, INPUT);
+  pinMode(ECHO_REAR, INPUT);
+
+  drive_index = 0;
 }
+
 void loop() 
 {
-  switch (drive_mode) {
+  timer = timer + 1;
+  if(timer > 100)
+  {
+    timer = 0;
+    timeup = true;
+  }
+
+  switch(drive_index)
+  {
     case 0:
     {
-      lift.write(lift_lower);
-      pulley_arm.write(pulley_side);
-      digitalWrite(WHEELS, HIGH);
-
-      if(abs(sonar - getSonar()) >= sonar_jump)
+      if(digitalRead(BUTTON) == HIGH)
       {
-        digitalWrite(WHEELS, LOW);
-        drive_mode = 1;
+        drive_index = 1;
+        ping_jump = getSonar(TRIGGER_REAR, ECHO_REAR);
       }
+      break;
     }
-    case 1:
+    case 1: //Drive to ledge
     {
-      write.lift(lift_raise);
+      digitalWrite();
+      digitalWrite();
 
-      delay(100);
-
-      write.pulley_arm(pulley_centre);
-
-      delay(100);
-
-      write.lift(lift_lower);
-      drive_mode = 2;
-    }
-    case 2:
-    {
-      digitalWrite(PULLEYS, HIGH);
-
-      delay(500);
-
-      for (posVal = 0; digitalRead(CONTACT) == HIGH; posVal += 1) 
+      if(timeup = true)
       {
-        // in steps of 1 degree
-        lift.write(posVal);       // tell servo to go to position in variable 'pos'
-        delay(1);                   // waits 15ms for the servo to reach the position
+        timeup = false;
+        if(abs(ping_jump - getSonar(TRIGGER_REAR, ECHO_REAR)) >= jump_size)
+        {
+          drive_index = 2;
+          digitalWrite();
+          digitalWrite();
+        }
+        ping_jump = getSonar(TRIGGER_REAR, ECHO_REAR);
       }
-      digitalWrite(WHEELS, HIGH);
+      break;
+    }
+    case 2: //Centre pulley over rope
+    {
+      Bot.ToPosition(SERVO_L1, Lift1_Raised);
+      Bot.ToPosition(SERVO_L2, Lift2_Raised);
 
-      delay(500);
+      if(timeup = true)
+      {
+        drive_index = 3;
+        timeup = false;
+        ping_jump = getSonar(TRIGGER_FRONT, ECHO_FRONT);        
+        
+        Bot.ToPosition(SERVO_R, Rack_Centre);
+      }
+      break;
+    }
+    case 3: //Drive along rope
+    {
+      Bot.ToPosition(SERVO_L1, Lift1_Lowered);
+      Bot.ToPosition(SERVO_L2, Lift2_Lowered);
 
-      digitalWrite(WHEELS,LOW);
-      digitalWrite(PULLEYS, LOW);
-      write.lift(lift_raise);
+      digitalWrite();
+      digitalWrite();
+
+      if(timeup = true)
+      {
+        timeup = false;
+        if(abs(ping_jump - getSonar(TRIGGER_FRONT, ECHO_FRONT)) >= jump_size)
+        {
+          drive_index = 4;
+        }
+        ping_jump = getSonar(TRIGGER_FRONT, ECHO_FRONT);
+      }
+      break;
+    }
+    case 4: //Land on other table
+    {
+      Bot.ToPosition(SERVO_L1, Lift1_Raised);
+      Bot.ToPosition(SERVO_L2, Lift2_Raised);
+
+      digitalWrite();
+      digitalWrite();
       
-      delay(50);
-
-      write.pulley_arm(pulley_side);
-
-      delay(50);
-
-      write.lift(lift_lower);
-
-      drive_mode 3;
-    }
-    case 3:
-    {
-      digitalWrite(WHEELS, HIGH);
-
-      if(digitalRead(CONTACT) == LOW)
+      if(timeup = true)
       {
-        digitalWrite(WHEELS, LOW);
-        drive_mode 4;
+        drive_index = 5;
+        timeup = false;       
+        
+        Bot.ToPosition(SERVO_R, Rack_SIDE);
+        digitalWrite();
+        digitalWrite();
+        ping_jump = getSonar(TRIGGER_FRONT, ECHO_FRONT);
       }
+      break;
     }
-    case 4:
+    case 5: //Drive to ledge
     {
-      digitalWrite(WHEELS, REVERSE);
-      
-      delay(reverse_time);
+      if(timeup = true)
+      {
+        timeup = false;
+        if(abs(ping_jump - getSonar(TRIGGER_FRONT, ECHO_FRONT)) >= jump_size)
+        {
+          drive_index = 6;
+          digitalWrite();
+          digitalWrite();
+        }
+        ping_jump = getSonar(TRIGGER_FRONT, ECHO_FRONT);
+      }
+      break;
     }
-  
+    case 6:
+    {
+      digitalWrite();
+      digitalWrite();
+
+      delay(reverse_distance / drive_speed);
+
+      digitalWrite();
+      digitalWrite();
+
+      digitalWrite(LED, HIGH);
+
+      delay(1000);
+
+      drive_index = 0;
+    }    
   }
 }
 
-float getSonar() {
+unsigned long getSonar(int trigger, int echo) 
+{
   unsigned long pingTime;
-  float distance;
-  // make trigPin output high level lasting for 10μs to triger HC_SR04
-  digitalWrite(TRIGGER, HIGH); 
+  digitalWrite(trigger, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER, LOW);
-  // Wait HC-SR04 returning to the high level and measure out this waitting time
-  pingTime = pulseIn(ECHO, HIGH, 420000); 
-  return pingTime; // return the distance value
+  digitalWrite(trigger, LOW);
+  pingTime = pulseIn(echo, HIGH, 42000);
+  return pingTime;
 }
